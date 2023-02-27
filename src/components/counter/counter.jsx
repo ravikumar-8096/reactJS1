3ba538701902fa6b31a1d110eb489b02da7b85d1
ravikumar8096 <>
2023-02-27 20:05:06 +0530

    sign-changes

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
+import SignaturePad from "react-signature-canvas";
 
-let Counter = ({add, sub, double}) => {
-    const [counter, setCounter] = React.useState(0);
-    let handleCounter = (value) => {
-        setCounter(counter + value)
-    }
+let Counter = ({add}) => {
+    // const [counter, setCounter] = React.useState(0);
+    // let handleCounter = (value) => {
+    //     // setCounter(counter + value)
+    // }
+    let signPad = useRef({});
+    let data = "";
+    const clear = () => {
+      signPad.current.clear();
+    };
+    const save = () => {
+      data = signPad.current.toDataURL();
+      console.log('ravi-dat--',data);
+    };
+    const show = () => {
+      signPad.current.fromDataURL(data);
+      console.log('ravi-show--',signPad.current.fromDataURL(data));
+
+    };
     return (
         <>
             <div className="container-fluid text-center mt-5">
-                <h1 className="text-uppercase text-info h1 ">Counters</h1>
-                <div className="text-success display-3 my-4  font-weight-bold">{counter}</div>
-                <button className="btn btn-primary mx-2 border border-success" onClick={() => handleCounter(add)}>Increment</button>
-                <button className="btn btn-danger mx-2 border border-primary" onClick={() => handleCounter(sub)}>Decrement</button>
-                <button className="btn btn-warning mx-2 border border-danger" onClick={() => handleCounter(double)}>Double</button>
-            </div>
+                <h1 className="text-uppercase text-info h1 ">Sign</h1>
+                {/* <div className="text-success display-3 my-4  font-weight-bold">{counter}</div> */}
+                {/* <button className="btn btn-primary mx-2 border border-success" onClick={() => handleCounter(add)}>Submit</button> */}
+                <SignaturePad penColor='green' toDataURL
+    canvasProps={{width: 500, height: 200, className: 'sigCanvas'}} 
+    ref={signPad}
+    />
+     <button onClick={clear}>Clear</button>
+        <button onClick={save}>Save</button>
+        <button onClick={show}>Show</button>
+                 </div>
             
         </>
     );
